f9b19b3a0440d2a31497f7ff74b52e793f0e4b06
Mathieu Gardere <wam@mathieugardere.com>
2014-06-13 10:54:25 +1000

    More iOS client goodness

---
+++ src/clients/iOS client/ChouFleurClient/ChouFleurClient/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  ChouFleurClient
+//
+//  Created by Mathieu Gardere on 13/06/2014.
+//  Copyright (c) 2014 Mathieu Gardere. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
